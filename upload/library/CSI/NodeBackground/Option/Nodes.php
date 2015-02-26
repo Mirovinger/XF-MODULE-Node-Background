1e250fed90b70e3f00749053f8eca5ea7bd28abd
Kitsune Solar <github01@mlbox.org>
2015-02-26 15:50:17 +0300

    Update Nodes.php

--- upload/library/CSI/NodeBackground/Option/Nodes.php
+++ upload/library/CSI/NodeBackground/Option/Nodes.php
@@ -1,38 +1,14 @@
 <?php
 
-class CSI_NodeBackground_Option_Nodes
+class CSI_NodeBackground_Option_Nodes extends XenForo_Option_NodeChooser
 {
-  public static function renderOption(XenForo_View $view, $fieldPrefix, array $preparedOption, $canEdit)
+  public static function renderNodeChooser(XenForo_View $view, $fieldPrefix, array $preparedOption, $canEdit)
   {
-    $nodeModel = XenForo_Model::create('CSI_NodeBackground_Model_GetNodes');
-    $preparedOption['formatParams'] = $nodeModel->getNodesOptions(
-      $preparedOption['option_value']
-    );
-    return XenForo_ViewAdmin_Helper_Option::renderOptionTemplateInternal(
-      'option_list_option_checkbox',
-      $view, $fieldPrefix, $preparedOption, $canEdit
-    );
+    $preparedOption['nodeFilter'] = 'Forum';
+    return self::_render('csiXF_nodeBg_optionList_optionMultiple', $view, $fieldPrefix, $preparedOption, $canEdit);
   }
-
-  public static function renderMultiple(XenForo_View $view, $fieldPrefix, array $preparedOption, $canEdit)
+  public static function getOption($key)
   {
-    $value = $preparedOption['option_value'];
-    $editLink = $view->createTemplateObject('option_list_option_editlink', array(
-      'preparedOption' => $preparedOption,
-      'canEditOptionDefinition' => $canEdit
-    ));
-
-    $nodeModel = XenForo_Model::create('CSI_NodeBackground_Model_GetNodes');
-    $forumOptions = $nodeModel->getNodesOptions(
-      $preparedOption['option_value'],
-      '(unspecified)'
-    );
-    return $view->createTemplateObject('csiXF_nodeBg_optionList_optionMultiple', array(
-      'fieldPrefix' => $fieldPrefix,
-      'listedFieldName' => $fieldPrefix . '_listed[]',
-      'preparedOption' => $preparedOption,
-      'formatParams' => $forumOptions,
-      'editLink' => $editLink
-    ));
+    return XenForo_Application::getOptions()->get(sprintf('csiXF_%s', $key));
   }
 }
